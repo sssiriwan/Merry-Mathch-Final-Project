bfb34ec4b910efa4dc2dbe2bc0cbf16473412708
sssiriwan <siriwan9933@gmail.com>
2023-09-02 13:26:31 +0700

    feat:landing-page-done

---
+++ client/src/components/base/cards/cardsMockData/data.js
@@ -0,0 +1,27 @@
+export let datas = [
+    {
+      id: 1,
+      image: "./icons/emoji-1.png",
+      text1: "Upload your cool",
+      text2: "picture",
+    },
+    {
+      id: 2,
+      image: "./icons/emoji-2.png",
+      text1: "Explore and find",
+      text2: "the one you like",
+    },
+    {
+      id: 3,
+      image: "./icons/emoji-3.png",
+      text1: "Click ‘Merry’ for",
+      text2: "get to know!",
+    },
+    {
+      id: 4,
+      image: "./icons/emoji-4.png",
+      text1: "Start chating and",
+      text2: "relationship",
+    },
+  ];
+  
