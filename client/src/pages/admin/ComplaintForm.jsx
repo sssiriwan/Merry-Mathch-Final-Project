1eb6fe9ce707761c53c4383eeaa62b9b60bbe87f
sssiriwan <siriwan9933@gmail.com>
2023-09-02 12:54:00 +0700

    feat:add-typography-component-and-complaintform

---
+++ client/src/pages/admin/ComplaintForm.jsx
@@ -0,0 +1,12 @@
+import { TypographyH1 } from "@/components/base/button/Typography";
+function ComplaintForm() {
+  return (
+    <>
+      <small>COMPLAINT</small>
+      <TypographyH1 title="If you have any trouble" />
+      <TypographyH1 title="Don't be afraid to tell us!" />
+    </>
+  );
+}
+
+export default ComplaintForm;
