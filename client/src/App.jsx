f40efd61ba00fea006546220f72d649c043810c3
Bank <blankinfear@gmail.com>
2023-09-03 13:03:10 +0700

    feat:seet up PaymentPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ComplaintDetailPage from "./pages/ComplaintDetailPage";
 import Package from "./pages/Package";
+import Payment from "./pages/Payment";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/admin" element={<Home />} />
-        <Route path="/" element={<Package />} />
-        <Route path="/aaa" element={<ComplaintDetailPage />} />
+        <Route path="/aaa" element={<Home />} />
+        <Route path="/aaaa" element={<Package />} />
+        <Route path="/" element={<Payment />} />
+        <Route path="/admin" element={<ComplaintDetailPage />} />
       </Routes>
     </BrowserRouter>
   );
