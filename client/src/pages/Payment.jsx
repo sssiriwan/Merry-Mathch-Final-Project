7f55e122dc492e1738392802a81da8be588223cf
Bank <blankinfear@gmail.com>
2023-09-03 14:58:51 +0700

    feat: add paymentpage

--- client/src/pages/Payment.jsx
+++ client/src/pages/Payment.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "@/components/base/Navbar";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 const Payment = () => {
   return (
     <div>
@@ -7,7 +10,14 @@ const Payment = () => {
       <div className="w-screen h-[936px] flex justify-center">
         <div className="w-[928px] h-[554px] mt-[80px] ml- border">
           <div className="w-[358px] h-[244px] border"></div>
-          <div></div>
+          <div>
+            <Label htmlFor="email">Card number</Label>
+            <Input
+              type="Card Number"
+              id="Card Number"
+              placeholder="Number of card"
+            />
+          </div>
         </div>
       </div>
     </div>
