ab511cb7062bde24408408d7ff7d94e1ebae6b8e
Bank <blankinfear@gmail.com>
2023-09-06 00:08:53 +0700

    feat: add paymemt page 1, 2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ComplaintDetailPage from "./pages/ComplaintDetailPage";
 import Package from "./pages/Package";
-import Payment from "./pages/Payment";
+import Payment_1 from "./pages/Payment_1";
+import Payment_2 from "./pages/Payment_2";
 import ComplaintFormPage from "./pages/ComplaintFormPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPageStep1 } from "./pages/RegisterPageStep1";
@@ -18,7 +19,8 @@ function App() {
         <Route path="/register" element={<Form />} />
         <Route path="/aaaa" element={<Home />} />
         <Route path="/aaa" element={<Package />} />
-        <Route path="/" element={<Payment />} />
+        <Route path="/payment_1" element={<Payment_1 />} />
+        <Route path="/payment_2" element={<Payment_2 />} />
         <Route path="/admin" element={<ComplaintDetailPage />} />
         <Route path="/complaint" element={<ComplaintFormPage />} />
         <Route path="/login" element={<LoginPage />} />
