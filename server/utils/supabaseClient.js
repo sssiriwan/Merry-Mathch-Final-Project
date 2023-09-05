090cc9688e8329ebce80dc87db5fe16e80a08e42
PloyIIV <chadapan.y58@gmail.com>
2023-09-05 12:39:51 +0700

    feat: connect backend to supabase

--- server/utils/supabaseClient.js
+++ server/utils/supabaseClient.js
@@ -1,16 +1,16 @@
 import dotenv from "dotenv"
-import * as pg from "pg"
+// import * as pg from "pg"
 
 dotenv.config();
-const { Pool } = pg.default;
+// const { Pool } = pg.default;
 
-const pool = new Pool({
-    connectionString: `postgresql://postgres:${process.env.SUPABASE_PASSWORD}@db.pauqbkgvjpahoowveuka.supabase.co:5432/postgres`
-});
+// const pool = new Pool({
+//     connectionString: `postgresql://postgres:${process.env.SUPABASE_PASSWORD}@db.pauqbkgvjpahoowveuka.supabase.co:5432/postgres`
+// });
 
-export { pool }
+// export { pool }
 
-// import { createClient } from '@supabase/supabase-js'
-// const supabaseUrl = process.env.SUPABASE_URL
-// const supabaseKey = process.env.SUPABASE_ANON_KEY
-// export const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl, supabaseKey )
