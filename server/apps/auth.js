8fb9e6ba80bc14434149562b152a57c5e8fe4b54
PloyIIV <chadapan.y58@gmail.com>
2023-09-06 10:34:18 +0700

    feat: add jsonwebtoken on register router

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -3,72 +3,95 @@ import { Router } from "express";
 import { supabase } from "../utils/supabaseClient.js";
 // import multer from "multer"
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 // const multerUpload = multer({ dest: "uploads/"});
 // const avatarUpload = multerUpload.fields([{name: "avatar", maxCount:5 }]);
-
+// const {data, error} = await supabase.storage.from('users').download(path)
 authRouter.post("/register", async (req, res) => {
-  const user = {
-    username: req.body.username,
-    password: req.body.password,
-    // avatar_url: req.files.avatar,
-    fullname: req.body.fullname,
-    role: "Users",
-    email: req.body.email,
-    location: req.body.location,
-    age: req.body.age,
-    hobbies: req.body.hobbies,
-    created_at: new Date(),
-  };
-  const salt = await bcrypt.genSalt(10);
-  user.password = await bcrypt.hash(user.password, salt);
-  await supabase
-    .from("users")
-    .insert([
-      {
-        username: user.username,
-        password: user.password,
-        fullname: user.fullname,
-        role: user.role,
-        email: user.email,
-        age: user.age,
-        created_at: user.created_at,
-        // location: user.location,
-      },
-    ])
-    .select();
-  return res.json({
-    message: `Created new account successfully`,
-  });
+  try {
+    const user = {
+      username: req.body.username,
+      password: req.body.password,
+      // avatar_url: req.files.avatar,
+      fullname: req.body.fullname,
+      role: "Users",
+      email: req.body.email,
+      location: req.body.location,
+      age: req.body.age,
+      hobbies: req.body.hobbies,
+      created_at: new Date(),
+    };
+    // const avatarFile = req.files.avatar
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+    await supabase
+      .from("users")
+      .insert([
+        {
+          user,
+        },
+      ])
+      .select();
+      // ต้องเปลี่ยนชื่อ file ตรง upload
+    // const { data,error } = await supabase.storage.from('avatars').upload('public/avatar1.png', avatarFile);
+    return res.json({
+      message: `Created new account successfully`,
+      // image: data
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-authRouter.post("/login", async (req,res) => {
-  const {data: user, error} = await supabase.from('users').select('*').eq('username', req.body.username);
-  if(!user[0]) {
-    return res.status(404).json({
-      message: "User not found"
-    })
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { data: user, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("username", req.body.username);
+    if (error) {
+      console.log(error);
+    }
+    if (!user[0]) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user[0].password
+    );
+    if (!isValidPassword) {
+      return res.status(401).json({
+        message: "Password is invalid",
+      });
+    }
+    const token = jwt.sign(
+      { id: user[0].id, username: user[0].username },
+      process.env.SUPABASE_JWT_KEY,
+      { expiresIn: "900000" }
+    );
+    return res.json({
+      message: user[0],
+      token,
+    });
+  } catch (error) {
+    console.log(error);
   }
-  const isValidPassword = await bcrypt.compare(req.body.password, user[0].password)
-  if(!isValidPassword) {
-    return res.status(401).json({
-      message: "Password is invalid"
-    })
-  }
-  return res.json({
-    message: user[0],
-  })
-})
-
+});
 
 authRouter.get("/", async (req, res) => {
-  const result = await supabase.from("users").select("*");
-  return res.json({
-    data: result,
-  });
+  try {
+    const result = await supabase.from("users").select("*");
+    return res.json({
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
-
 
 export default authRouter;
 
