b56902fb07b1f054a19865af85ae22c0385ffb2b
PloyIIV <chadapan.y58@gmail.com>
2023-09-21 22:17:05 +0700

    feat: add previewCard on matchingPage

--- client/src/pages/MatchingPage.jsx
+++ client/src/pages/MatchingPage.jsx
@@ -26,12 +26,14 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
+import PreviewCard from "./PreviewCard";
 
 function MatchingPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [profile, setProfile] = useState({});
   const [profileImg, setProfileImg] = useState({});
   const [count, setCount] = useState(0);
+  const [clicked, setClicked] = useState(false);
 
   const getData = async () => {
     setIsLoading(true);
@@ -67,11 +69,11 @@ function MatchingPage() {
   const age = (birthday) => {
     const today = new Date();
     const userAge =
-    today.getFullYear() -
-    birthday.getFullYear() -
-    (today.getMonth() < birthday.getMonth() ||
-    (today.getMonth() === birthday.getMonth() &&
-    today.getDate() < birthday.getDate()));
+      today.getFullYear() -
+      birthday.getFullYear() -
+      (today.getMonth() < birthday.getMonth() ||
+        (today.getMonth() === birthday.getMonth() &&
+          today.getDate() < birthday.getDate()));
     return userAge;
   };
 
@@ -236,6 +238,43 @@ function MatchingPage() {
           </div>
         </aside>
         <section className="w-[72%] bg-putility-400 flex justify-center items-center">
+          {clicked && (
+            <div className="absolute left-[500px] top-40 z-20">
+              <PreviewCard
+                setClicked={setClicked}
+                clicked={clicked}
+                userId={profile.user_id}
+              />
+            </div>
+          )}
+          <div className=" w-fit absolute left-[700px] bottom-[140px] z-10 flex justify-between items-center px-10">
+            <div className="flex font-bold text-3xl text-white items-center">
+              <h1>{profile.fullname}</h1>
+              <h2 className="ml-2 text-pgray-400">
+                {age(new Date(profile.date_of_birth))}
+              </h2>
+              <button onClick={() => { setClicked(!clicked) }} className="rounded-full w-7 h-7 flex justify-center items-center ml-2 bg-white bg-opacity-30 hover:bg-opacity-10">
+                <svg
+                  width="16"
+                  height="16"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8 10C8.53043 10 9.03914 9.78929 9.41421 9.41421C9.78929 9.03914 10 8.53043 10 8C10 7.46957 9.78929 6.96086 9.41421 6.58579C9.03914 6.21071 8.53043 6 8 6C7.46957 6 6.96086 6.21071 6.58579 6.58579C6.21071 6.96086 6 7.46957 6 8C6 8.53043 6.21071 9.03914 6.58579 9.41421C6.96086 9.78929 7.46957 10 8 10Z"
+                    fill="white"
+                  />
+                  <path
+                    fill-rule="evenodd"
+                    clip-rule="evenodd"
+                    d="M0.531497 8.47203C0.41398 8.16668 0.41398 7.82858 0.531497 7.52323C1.11328 6.01482 2.13826 4.71798 3.47146 3.80347C4.80467 2.88896 6.38358 2.39967 8.0003 2.40002C11.4059 2.40002 14.3147 4.52802 15.4691 7.52803C15.5867 7.83283 15.5859 8.17123 15.4691 8.47683C14.8873 9.98523 13.8623 11.2821 12.5291 12.1966C11.1959 13.1111 9.61701 13.6004 8.0003 13.6C4.5947 13.6 1.6859 11.472 0.531497 8.47203ZM11.2003 8.00003C11.2003 8.84872 10.8632 9.66265 10.263 10.2628C9.66292 10.8629 8.84899 11.2 8.0003 11.2C7.1516 11.2 6.33767 10.8629 5.73755 10.2628C5.13744 9.66265 4.8003 8.84872 4.8003 8.00003C4.8003 7.15133 5.13744 6.3374 5.73755 5.73728C6.33767 5.13717 7.1516 4.80002 8.0003 4.80002C8.84899 4.80002 9.66292 5.13717 10.263 5.73728C10.8632 6.3374 11.2003 7.15133 11.2003 8.00003Z"
+                    fill="white"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
           <Swiper
             slidesPerView={"auto"}
             centeredSlides={true}
@@ -247,32 +286,6 @@ function MatchingPage() {
             modules={[Pagination, Navigation]}
             className="mySwiper relative"
           >
-            <div className=" w-fit absolute left-1/4 bottom-20 z-10 flex justify-between items-center px-10">
-              <div className="flex font-bold text-3xl text-white items-center">
-                <h1>{profile.fullname}</h1>
-                <h2 className="ml-2 text-pgray-400">{age(new Date(profile.date_of_birth))}</h2>
-                <button className="rounded-full w-7 h-7 flex justify-center items-center ml-2 bg-white bg-opacity-30 hover:bg-opacity-10">
-                  <svg
-                    width="16"
-                    height="16"
-                    viewBox="0 0 16 16"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8 10C8.53043 10 9.03914 9.78929 9.41421 9.41421C9.78929 9.03914 10 8.53043 10 8C10 7.46957 9.78929 6.96086 9.41421 6.58579C9.03914 6.21071 8.53043 6 8 6C7.46957 6 6.96086 6.21071 6.58579 6.58579C6.21071 6.96086 6 7.46957 6 8C6 8.53043 6.21071 9.03914 6.58579 9.41421C6.96086 9.78929 7.46957 10 8 10Z"
-                      fill="white"
-                    />
-                    <path
-                      fill-rule="evenodd"
-                      clip-rule="evenodd"
-                      d="M0.531497 8.47203C0.41398 8.16668 0.41398 7.82858 0.531497 7.52323C1.11328 6.01482 2.13826 4.71798 3.47146 3.80347C4.80467 2.88896 6.38358 2.39967 8.0003 2.40002C11.4059 2.40002 14.3147 4.52802 15.4691 7.52803C15.5867 7.83283 15.5859 8.17123 15.4691 8.47683C14.8873 9.98523 13.8623 11.2821 12.5291 12.1966C11.1959 13.1111 9.61701 13.6004 8.0003 13.6C4.5947 13.6 1.6859 11.472 0.531497 8.47203ZM11.2003 8.00003C11.2003 8.84872 10.8632 9.66265 10.263 10.2628C9.66292 10.8629 8.84899 11.2 8.0003 11.2C7.1516 11.2 6.33767 10.8629 5.73755 10.2628C5.13744 9.66265 4.8003 8.84872 4.8003 8.00003C4.8003 7.15133 5.13744 6.3374 5.73755 5.73728C6.33767 5.13717 7.1516 4.80002 8.0003 4.80002C8.84899 4.80002 9.66292 5.13717 10.263 5.73728C10.8632 6.3374 11.2003 7.15133 11.2003 8.00003Z"
-                      fill="white"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
             {!isLoading && (
               <>
                 {Object.values(profileImg).map((image) => {
