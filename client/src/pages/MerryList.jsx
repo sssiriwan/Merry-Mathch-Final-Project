ae6fd9cc691a2e5b1ae7d4baba73aeb6f5b2de84
sssiriwan <siriwan9933@gmail.com>
2023-09-22 12:07:51 +0700

    feat:function-unmerry

--- client/src/pages/MerryList.jsx
+++ client/src/pages/MerryList.jsx
@@ -16,6 +16,7 @@ function MerryCard(props) {
 
   const [clicked, setClicked] = useState(false);
   const profile = props.user;
+  // const [updateStatus, setUpdateStatus] = useState(profile.status)
 
   const age = (birthday) => {
     const today = new Date();
@@ -26,6 +27,36 @@ function MerryCard(props) {
         (today.getMonth() === birthday.getMonth() &&
           today.getDate() < birthday.getDate()));
     return userAge;
+  };
+
+  const handleMatchClick = async (item) => {
+    try {
+
+      // if (item.status === "merry"){
+        const updateStatus = {
+        match_list_id: item.match_list_id,
+        status: "unmerry",
+      };
+    //   return updateStatus
+    // }
+  //   if (item.status === "unmerry"){
+  //     const updateStatus = {
+  //     match_list_id: item.match_list_id ,
+  //     status: "merry",
+  //   };
+  //   return updateStatus
+  // }   
+      
+      const result = await axios.put(
+        `http://localhost:4000/post/match`,
+        updateStatus
+      );
+
+      //  Navigate to the complaint detail page
+     // navigate(`/admin/complain/detail/${param.complainId}`);
+    } catch (error) {
+      alert(error);
+    }
   };
 
   const merryMatchButton = (status) => {
@@ -84,6 +115,26 @@ function MerryCard(props) {
     }
   }
 
+  const merryButtonStyle = (status) => {
+    if(status.toLowerCase() === "merry"){
+      const style = "w-10 h-10 rounded-2xl shadow-3xl bg-pred-500";
+      return style
+    }else{
+      const style = "w-10 h-10 rounded-2xl shadow-3xl bg-white";
+      return style
+    }
+  }
+
+  const heartStyle = (status) => {
+    if(status.toLowerCase() === "merry"){
+      const style = "white";
+      return style
+    }else{
+      const style = "red";
+      return style
+    }
+  }
+
 
 
   return (
@@ -134,57 +185,10 @@ function MerryCard(props) {
         </div>
         <div className="w-[200px] h-[180px] flex flex-col items-end">
         {merryMatchButton(profile.status)}
-          {/* Merry match! เดี๋ยวมาทำ logic */}
-          {/* เช็ค status แล้วเลือกแสดงปุ่ม เมื่อคลิกแล้วให้เปลี่ยน state status */}
-          {/* <div className="border-2 border-pred-500 rounded-full flex items-center justify-center py-1 px-4 w-fit">
-            <svg
-              width="21"
-              height="12"
-              viewBox="0 0 21 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g clip-path="url(#clip0_5071_276)">
-                <path
-                  d="M6.61163 10.155C6.84858 10.3046 7.09033 10.4464 7.33649 10.5804L7.33646 10.5804L7.34158 10.5831L7.34904 10.5876L7.36365 10.5949C7.4981 10.6645 7.64737 10.7008 7.79885 10.7006C7.90344 10.7004 8.00692 10.6829 8.10516 10.649H8.14476L8.25354 10.5837L8.26054 10.5799C9.09099 10.1257 9.87224 9.58696 10.5918 8.97212L10.5918 8.97212L10.5935 8.97069C11.7169 8.00225 13.098 6.44469 13.098 4.49998V4.49993C13.098 3.83821 12.8928 3.19279 12.5107 2.65253C12.1286 2.11226 11.5885 1.70374 10.9646 1.48319C10.3407 1.26265 9.66378 1.24094 9.02704 1.42105C8.57122 1.54999 8.15204 1.77782 7.79805 2.08569C7.44406 1.77782 7.02487 1.54999 6.56905 1.42105C5.93232 1.24094 5.25541 1.26265 4.63152 1.48319C4.00764 1.70374 3.46747 2.11226 3.08539 2.65253C2.70331 3.19279 2.49811 3.83821 2.49805 4.49993V4.49998C2.49805 6.44474 3.87983 8.00231 5.00248 8.9706L5.00421 8.97208C5.5106 9.40485 6.04783 9.80019 6.61163 10.155ZM6.61163 10.155C6.61152 10.1549 6.61141 10.1548 6.6113 10.1547L6.8782 9.73204L6.61197 10.1552C6.61186 10.1551 6.61174 10.155 6.61163 10.155Z"
-                  fill="#FF1659"
-                  stroke="white"
-                />
-              </g>
-              <g clip-path="url(#clip1_5071_276)">
-                <path
-                  d="M13.8128 10.155C14.0498 10.3046 14.2915 10.4464 14.5377 10.5804L14.5376 10.5804L14.5427 10.5831L14.5502 10.5876L14.5648 10.5949C14.6993 10.6645 14.8485 10.7008 15 10.7006C15.1046 10.7004 15.2081 10.6829 15.3063 10.649H15.3459L15.4547 10.5837L15.4617 10.5799C16.2922 10.1257 17.0734 9.58696 17.793 8.97212L17.793 8.97212L17.7947 8.97069C18.9181 8.00225 20.2992 6.44469 20.2992 4.49998V4.49993C20.2992 3.83821 20.094 3.19279 19.7119 2.65253C19.3298 2.11226 18.7896 1.70374 18.1657 1.48319C17.5419 1.26265 16.8649 1.24094 16.2282 1.42105C15.7724 1.54999 15.3532 1.77782 14.9992 2.08569C14.6452 1.77782 14.226 1.54999 13.7702 1.42105C13.1335 1.24094 12.4566 1.26265 11.8327 1.48319C11.2088 1.70374 10.6686 2.11226 10.2866 2.65253C9.90448 3.19279 9.69928 3.83821 9.69922 4.49993V4.49998C9.69922 6.44474 11.081 8.00231 12.2037 8.9706L12.2054 8.97208C12.7118 9.40485 13.249 9.80019 13.8128 10.155ZM13.8128 10.155C13.8127 10.1549 13.8126 10.1548 13.8125 10.1547L14.0794 9.73204L13.8131 10.1552C13.813 10.1551 13.8129 10.155 13.8128 10.155Z"
-                  fill="#FF1659"
-                  stroke="white"
-                />
-              </g>
-              <defs>
-                <clipPath id="clip0_5071_276">
-                  <rect
-                    width="12"
-                    height="12"
-                    fill="white"
-                    transform="translate(1.79883)"
-                  />
-                </clipPath>
-                <clipPath id="clip1_5071_276">
-                  <rect
-                    width="12"
-                    height="12"
-                    fill="white"
-                    transform="translate(9)"
-                  />
-                </clipPath>
-              </defs>
-            </svg>
-            <p className="text-pred-500 font-extrabold ml-2">Merry Match!</p>
-          </div> */}
-          {/* Not match yet ร่วมกับด้านบน */}
-          {/* <div className="border-2 border-pgray-500 rounded-full flex items-center justify-center py-1 px-4 w-fit">
-          <p className="text-pgray-700">Not Match yet</p>
-          </div> */}
           <div className="w-10/12 justify-between flex flex-row-reverse mt-5">
-            <button className="w-10 h-10 bg-pred-500 rounded-2xl shadow-3xl">
+            {/* เช็ค status แล้วเลือกแสดงปุ่ม เมื่อคลิกแล้วให้เปลี่ยน state status */}
+            <button className={merryButtonStyle(profile.status)} onClick={() => handleMatchClick(profile)}>
+              
               <svg
                 width="44"
                 height="42"
@@ -195,7 +199,7 @@ function MerryCard(props) {
                 <g filter="url(#filter0_d_5071_59)">
                   <path
                     d="M19.584 27.298L19.578 27.2944L19.5552 27.2824C19.0817 27.0248 18.6166 26.7518 18.1608 26.464C17.074 25.7802 16.0385 25.0182 15.0624 24.184C12.8544 22.2796 10.4004 19.4224 10.4004 16C10.4005 14.8834 10.7468 13.7942 11.3915 12.8825C12.0363 11.9708 12.9478 11.2814 14.0006 10.9093C15.0534 10.5371 16.1957 10.5005 17.2702 10.8044C18.3447 11.1083 19.2985 11.7379 20.0004 12.6064C20.7022 11.7379 21.6561 11.1083 22.7306 10.8044C23.8051 10.5005 24.9473 10.5371 26.0001 10.9093C27.053 11.2814 27.9645 11.9708 28.6093 12.8825C29.254 13.7942 29.6003 14.8834 29.6004 16C29.6004 19.4224 27.1476 22.2796 24.9384 24.184C23.5516 25.3689 22.046 26.4072 20.4456 27.2824L20.4228 27.2944L20.4168 27.298H20.4144C20.2869 27.3656 20.1448 27.401 20.0005 27.4012C19.8563 27.4014 19.7141 27.3664 19.5864 27.2992L19.584 27.298Z"
-                    fill="white"
+                    fill={heartStyle(profile.status)}
                   />
                 </g>
                 <defs>
@@ -287,10 +291,12 @@ function MerryCard(props) {
 
 function MerryList() {
   const [user, setUser] = useState({});
+  // const [status, setStatus] = useState()
   const getUser = async () => {
     const result = await axios.get("http://localhost:4000/post/match-list");
     console.log(result.data.data);
     setUser(result.data.data);
+    // setStatus(result.data.data.status)
   };
 
   useEffect(() => {
