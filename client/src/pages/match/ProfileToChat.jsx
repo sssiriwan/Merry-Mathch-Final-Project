814dd09f3908797b109fc36d2f05266916583c0d
sssiriwan <siriwan9933@gmail.com>
2023-09-29 13:50:53 +0700

    fix:set-loading-sidebar

--- client/src/pages/match/ProfileToChat.jsx
+++ client/src/pages/match/ProfileToChat.jsx
@@ -29,7 +29,7 @@ const ProfileToChat = () => {
       .or(`chooser.eq.${userId},chosen_one.eq.${userId}`)
       .eq("status", "match");
     //console.log("match จะมาไหม", data);
-    setIsLoading(true);
+    setIsLoading(false);
     setMatchList(data);
   };
 
