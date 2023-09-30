047617775dcbffa823f6573d5b984c41466a18d5
SupakornIm <supakorn.imjai@gmail.com>
2023-09-30 21:19:12 +0700

    feat: fetch package data from database

--- client/src/pages/membership/WelcomeMessage.jsx
+++ client/src/pages/membership/WelcomeMessage.jsx
@@ -13,8 +13,8 @@ function WelcomeMessage() {
       </div>
       {/* button */}
       <div className="flex-row mt-20">
-        <button className="px-4 py-2 rounded-3xl  font-bold bg-pred-100  text-pred-600  hover:bg-red-600  ">Back to home</button>
-        <button className="px-4 py-2 rounded-3xl  font-bold ml-5 bg-pred-500 text-white  hover:bg-gray-400 ">Check Membership</button>
+        <button className="px-4 py-2 rounded-3xl  font-bold bg-pred-100  text-pred-600  hover:bg-red-600  "><a href="/">Back to home</a></button>
+        <button className="px-4 py-2 rounded-3xl  font-bold ml-5 bg-pred-500 text-white  hover:bg-gray-400 "><a href="/membership">Check Membership</a></button>
       </div>
     </div>
   );
