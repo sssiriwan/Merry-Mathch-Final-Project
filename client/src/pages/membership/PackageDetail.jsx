daae3460f270ce7895b87b420ad89df102197447
SupakornIm <supakorn.imjai@gmail.com>
2023-09-30 21:19:12 +0700

    feat: fetch package data from database

--- client/src/pages/membership/PackageDetail.jsx
+++ client/src/pages/membership/PackageDetail.jsx
@@ -1,22 +1,48 @@
 import React from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 function PackageDetail() {
+  const navigate = useNavigate()
+
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState(0)
+  const [limit, setLimit] = useState(0);
+  const [icon, setIcon] = useState({});
+
+  const getMembershipData = async () => {
+    const response = await axios.get(`http://localhost:4000/post/membership`);
+    if (response.data.data.length > 0) {
+      setName(response.data.data[0].merry_packages.package_name);
+      setPrice(response.data.data[0].merry_packages.price);
+      setLimit(response.data.data[0].merry_packages.package_limit);
+      setIcon(response.data.data[0].merry_packages.package_icon);
+    } else {
+      // Redirect to the package page when there's no data
+      navigate("/package"); // Replace "/package" with the actual route to your package page
+    }
+}
+
+
+useEffect(() => {
+  getMembershipData();
+}, []);
   return (
     <div className="flex-col mt-6 w-[357px] h-[450px]  p-8  rounded-[32px]  bg-gradient-to-br from-ppurple-800 to-ppurple-400 shadow-xl">
       {/* icon  */}
       <div className="flex-col">
-        <img className="w-[60px] h-[60px]" src="" alt="" />
+        <img className="w-[60px] h-[60px]" src={icon} alt="" />
 
         {/* ชื่อ ราคา */}
         <div className="mt-6">
-          <h1 className=" text-3xl  text-white">Premium</h1>
-          <h2 className="  text-xl text-white">THB 159 /Month</h2>
+          <h1 className=" text-3xl  text-white">{name}</h1>
+          <h2 className="  text-xl text-white">THB {price} /Month</h2>
         </div>
         {/* detail */}
         <div className="mt-6">
           <h3 className=" text-base  text-purple-100">‘Merry’ more than a daily limited</h3>
-          <h3 className="text-base text-purple-100">Up to 50 Merry per day</h3>
+          <h3 className="text-base text-purple-100">Up to {limit} Merry per day</h3>
         </div>
         <hr className=" mt-9" />
         <div className=" mt-6">
