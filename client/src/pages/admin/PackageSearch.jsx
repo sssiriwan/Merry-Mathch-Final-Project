19bcdf222e58876eac11ba4a86a3adcbef128caa
PloyIIV <chadapan.y58@gmail.com>
2023-10-01 12:48:54 +0700

    feat: add query param in /package router

--- client/src/pages/admin/PackageSearch.jsx
+++ client/src/pages/admin/PackageSearch.jsx
@@ -1,13 +1,14 @@
 import { Input } from "@/components/ui/input";
 import { ButtonDemo } from "@/components/base/button/Button";
 
-function PackageSearch() {
+function PackageSearch({ setSearchItems }) {
   return (
     <div>
       <div className="flex flex-row">
         <Input
           className="flex w-80 p-3 items-center gap-4 rounded-md border border-gray-300 bg-white mr-2"
           placeholder="Search..."
+          onChange={(event) => { setSearchItems(event.target.value) }}
         />
         <div>
           <ButtonDemo>
